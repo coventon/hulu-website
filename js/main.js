a4f64a1bc3c8830a5d0d011c22c503051a97412f
Roma <rnejuta@gmail.com>
2021-07-24 17:48:49 -0500

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 const modal = document.querySelector('.modal')
 const loginBtn = document.querySelector('.login-btn')
-const closeBtn = document.querySelector('.close')
+const closBtn = document.querySelector('.close')
 
 loginBtn.addEventListener('click', openModal)
-closeBtn.addEventListener('click', closeModal)
+closBtn.addEventListener('click', closeModal)
 window.addEventListener('click', outsideClick)
 
 function openModal() {
-  modal.style.display = 'block'
+
+    modal.style.display = 'block'
 }
 
 function closeModal() {
-  modal.style.display = 'none'
+    modal.style.display = 'none'
 }
 
 function outsideClick(e) {
-  if (e.target == modal) {
-    closeModal()
-  }
+
+    if (e.target == modal) {
+        closeModal()
+    }
+
+
 }
